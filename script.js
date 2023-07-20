4503e65e4e35e3835aec2cf3fc548cd1aee4dd47
Divya-BA <137373896+Divya-BA@users.noreply.github.com>
2023-07-20 13:25:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+let container=document.createElement("container");
+
+let h2=document.createElement("h2");
+
+container.append(h2);
+
+document.body.append(container);
+let countdown=10;
+setTimeout(()=>
+{
+    h2.innerHTML=countdown--;
+    setTimeout(()=>
+    {
+        h2.innerHTML=countdown--;
+        setTimeout(()=>
+        {
+            h2.innerHTML=countdown--;
+            setTimeout(()=>
+            {
+                h2.innerHTML=countdown--;
+                setTimeout(()=>
+                {
+                    h2.innerHTML=countdown--;
+                    setTimeout(()=>
+                    {
+                        h2.innerHTML=countdown--;
+                        setTimeout(()=>
+                        {
+                            h2.innerHTML=countdown--;
+                            setTimeout(()=>
+                            {
+                                h2.innerHTML=countdown--;
+                                setTimeout(()=>
+                                {
+                                    h2.innerHTML=countdown--;
+                                    setTimeout(()=>
+                                    {
+                                        h2.innerHTML=countdown--;
+                                        setTimeout(()=>
+                                        {
+                                            h2.innerHTML=countdown--;
+                                            setTimeout(()=>
+                                            {
+                                                h2.innerHTML="Happy Independence Day"
+                                            
+                            
+                
+                                            },1000)
+                
+                                        },1000)
+                            
+                
+                                   },1000)
+                            
+                
+                                },1000)
+                            
+                
+                            },1000)
+                
+                        },1000)
+                
+                    },1000)
+                
+                },1000)
+            },1000)
+        },1000)
+
+    },1000)
+},1000);
